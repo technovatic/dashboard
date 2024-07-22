0b40a5d9962a0298a9da68821c8a47e099c21b83
Vishal Kelur <123793502+technovatic@users.noreply.github.com>
2024-07-23 00:28:21 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AiOutlineFileText, AiOutlineInfoCircle } from 'react-icons/ai';
 import { MdOutlinePendingActions } from "react-icons/md";
 import { TiUpload, TiTick } from "react-icons/ti";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Line } from 'react-chartjs-2';
 import {
@@ -15,7 +18,12 @@ import {
   Title,
   Tooltip,
   Legend,
+  Filler,
 } from 'chart.js';
+
+import figma from '../assets/figma.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
 
 ChartJS.register(
   CategoryScale,
@@ -24,12 +32,31 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  Filler
 );
+
+const markerData = [
+  { position: [28.6139, 77.209], image: figma, title: 'Delhi' },
+  { position: [12.9716, 77.5946], image: react, title: 'Bangalore' },
+  { position: [40.7128, -74.006], image: python, title: 'New York' },
+  { position: [24.7136, 46.6753], image: figma, title: 'Saudi Arabia' },
+  { position: [22.5726, 88.3639], image: react, title: 'Kolkata' },
+  { position: [34.0837, 74.7973], image: python, title: 'Jammu and Kashmir' },
+];
+
+const createCustomIcon = (iconUrl) => {
+  return L.icon({
+    iconUrl,
+    iconSize: [15, 15], // Smaller size to reduce overlap
+    iconAnchor: [7.5, 15], // Adjust anchor as needed
+    popupAnchor: [0, -15], // Adjust popup anchor as needed
+  });
+};
 
 const Card = ({ title, Icon, number }) => {
   return (
-    <div className="m-5 p-4 rounded-xl shadow-lg flex items-center bg-white">
+    <div className="m-5 p-4 rounded-xl shadow-lg flex items-center bg-purple-50">
       <Icon className="text-3xl text-blue-500 mx-7" />
       <div className="flex flex-col items-start ml-4">
         <h2 className="text-2xl text-gray-400">{title}</h2>
@@ -72,7 +99,7 @@ const GraphCard = () => {
       },
       tooltip: {
         callbacks: {
-          label: function(context) {
+          label: function (context) {
             let label = context.dataset.label || '';
             if (label) {
               label += ': ';
@@ -88,8 +115,8 @@ const GraphCard = () => {
   };
 
   return (
-    <div className="m-5 p-4 rounded-xl shadow-lg bg-white flex flex-col w-full md:w-1/2">
-      <h2 className="text-2xl text-gray-400 mb-4 text-center">Income Graph</h2>
+    <div className="m-5 p-4 rounded-xl shadow-lg bg-purple-50 flex flex-col w-full md:w-1/2">
+      <h2 className="text-2xl text-gray-400 mb-4 text-center font font-semibold">Income Graph</h2>
       <div className="w-full">
         <Line data={data} options={options} />
       </div>
@@ -98,7 +125,6 @@ const GraphCard = () => {
 };
 
 const JobTable = () => {
-  // Example data (replace with your actual data)
   const jobs = [
     { id: 1, projectName: 'Project A', projectAssigned: 'John Doe', workCompleted: 75 },
     { id: 2, projectName: 'Project B', projectAssigned: 'Jane Smith', workCompleted: 50 },
@@ -108,8 +134,8 @@ const JobTable = () => {
   ];
 
   return (
-    <div className="m-5 p-4 rounded-xl shadow-lg bg-white w-full md:w-1/2">
-      <h2 className="text-2xl text-gray-400 mb-4 text-center">Jobs on Hand</h2>
+    <div className="m-5 p-4 rounded-xl shadow-lg bg-purple-50 w-full md:w-1/2">
+      <h2 className="text-2xl text-gray-400 mb-4 text-center font font-semibold">Jobs on Hand</h2>
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
@@ -127,7 +153,7 @@ const JobTable = () => {
             </th>
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+        <tbody className="bg-purple-50 divide-y divide-gray-200">
           {jobs.map((job, index) => (
             <tr key={job.id}>
               <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{index + 1}</td>
@@ -147,7 +173,6 @@ const JobTable = () => {
 };
 
 const RecentConnectionsCard = () => {
-  // Example data for recent connections (replace with your actual data)
   const recentConnections = [
     { id: 1, title: 'Connection A', skilledSector: 'Technology A' },
     { id: 2, title: 'Connection B', skilledSector: 'Technology B' },
@@ -158,8 +183,8 @@ const RecentConnectionsCard = () => {
   ];
 
   return (
-    <div className="m-5 p-4 rounded-xl shadow-lg bg-white w-full md:w-1/2">
-      <h2 className="text-2xl text-gray-400 mb-4 text-center">Recent Connections</h2>
+    <div className="m-5 p-4 rounded-xl shadow-lg bg-purple-50 w-full md:w-1/2">
+      <h2 className="text-2xl text-gray-400 mb-4 text-center bold font font-semibold">Recent Connections</h2>
       <table className="w-full">
         <thead className="bg-gray-50">
           <tr>
@@ -183,29 +208,44 @@ const RecentConnectionsCard = () => {
 };
 
 const TechnologyTrendingCard = () => {
-  const defaultPosition = [19.076, 72.8777]; // Default position for Mumbai
-  const [markerPositions, setMarkerPositions] = React.useState([defaultPosition]);
+  const defaultPosition = [12.9716, 77.5946]; // Default position for India
+  const mapBounds = [
+    [-82.8628, 135.0000], // Southwest coordinates (South)
+    [71.7069, 42.6043], // Northeast coordinates (North)
+    [66.0272, -169.7022], // Southeast coordinates (East)
+    [52.1307, -3.7837] // Northwest coordinates (West)
+  ];
 
-  const handleMapClick = (e) => {
-    const { lat, lng } = e.latlng;
-    setMarkerPositions([...markerPositions, [lat, lng]]);
+  const createCustomIcon = (image) => {
+    return L.icon({
+      iconUrl: image,
+      iconSize: [25, 25], // Size of the icon
+      iconAnchor: [12, 24], // Point of the icon which will correspond to marker's location
+      popupAnchor: [0, -24] // Point from which the popup should open relative to the iconAnchor
+    });
   };
 
   return (
-    <div className="m-5 p-4 rounded-xl shadow-lg bg-white w-full md:w-1/2">
-      <h2 className="text-2xl text-gray-400 mb-4 text-center">Technology Trending</h2>
+    <div className="m-5 p-4 rounded-xl shadow-lg bg-purple-50 w-full lg:w-1/2">
+      <h2 className="text-xl sm:text-2xl text-gray-400 mb-4 text-center font-semibold">Technology Trending</h2>
       <div className="flex justify-center">
-        <MapContainer center={defaultPosition} zoom={12} style={{ height: '400px', width: '100%' }} onClick={handleMapClick}>
+        <MapContainer
+          center={defaultPosition}
+          zoom={5}
+          style={{ height: '400px', width: '100%' }}
+          maxBounds={mapBounds}
+          maxBoundsViscosity={1.0} // Ensure the map is kept within bounds
+          minZoom={3} // Adjust minZoom to prevent zooming out too far
+        >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            maxZoom={18}
+            maxZoom={12}
+            noWrap={true} // Prevents the map from repeating
           />
-          {markerPositions.map((position, index) => (
-            <Marker key={index} position={position}>
-              <Popup>
-                Marker {index + 1}
-              </Popup>
+           {markerData.map((marker, index) => (
+            <Marker key={index} position={marker.position} icon={createCustomIcon(marker.image)}>
+              <Popup>{marker.title}</Popup>
             </Marker>
           ))}
         </MapContainer>
@@ -219,11 +259,11 @@ const Dashboard = () => {
     <div className="flex flex-wrap items-end max-w-screen min-h-screen p-4 bg-blue-50">
       <div className="flex items-end mb-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
-          <span className="mr-2">Download Report</span>
+          <span className="mr-2 font font-bold">Download Report</span>
           <TiUpload className="ml-2" />
         </button>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-12 lg:gap-16 w-full mb-4 space-x-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-1 md:gap-12 lg:gap-16 w-full mb-4 space-x-4 font font-semibold">
         <Card title="Projects" Icon={AiOutlineFileText} number={178} />
         <Card title="Pending" Icon={MdOutlinePendingActions} number={50} />
         <Card title="Completed" Icon={TiTick} number={100} />
